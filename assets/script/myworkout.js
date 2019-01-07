08d4492f857c325695132718b7aac7fbd0749201
AngelMD55 <angel_demerutis@hotmail.com>
2019-01-06 17:14:43 -0700

    added Modal close button

--- assets/script/myworkout.js
+++ assets/script/myworkout.js
@@ -1,6 +1,32 @@
 displayNavInfo()
 
-const Workouts = ['Crossfit', 'Freeweights', 'Power Lifting']
+const Workouts = ['--', 'Crossfit', 'Freeweights'];
+// const Crossfit = [ 'Squats', 'Press', 'Lift', 'Olympic Lifting', 'Gymnastics', 'Other'];
+// const Squats = ['Air Squat', 'Front Squat','Overhead Squat','Back Squat', 'Back Squat'];
+
+
+
+const CrossfitArr = {
+    'Squats': ['Air Squat', 'Front Squat', 'Overhead Squat', 'Back Squat', 'Back Squat'],
+    'Press': ['Push Press', 'Push Jerk', 'Shoulder Press'],
+    'Lift': ['Deadlift', 'Sumo DeadLift', 'Medicine-Ball Clean'],
+    'Olympic Lifting': ['Clean', 'Power Clean', 'Hang-Clean', 'Snatch', 'Power Snatch', 'Dumbbell Snatch', 'Cluster'],
+    'Gymnastics': ['Push-Up', 'Sit-up', 'Pull-Up', 'Ring Row', 'Chest To Bar', 'Lsit', 'Lsit Pull Up', 'Box Jump', 'Ring Dip', 'Burpee', 'Toes To Bar', 'Hand Stand Push Up', 'Kipping', 'Rope Climb', 'Legless Rope Climb'],
+    'Other': ['Row', 'Truster', 'Jump Ropes', 'Double Unders', 'Wall Balls', 'Muscle-Up']
+}
+
+// console.log(CrossfitArr)
+
+// 'Freeweights': [{
+//     'Chest': ['Flat Barbell Bench Press', 'Incline Barbell Bench Press', 'Decline Barbell Bench Press', 'Flat Dumbbell Bench Press', 'Incline Dumbell Bench Press', 'Decline Dumbbell Bench Press', 'Flat Chest Press Machine', 'Incline Chest Press Machine', 'Decline Chest Press Machine', 'Dips', 'Push Up', 'Flat Dumbell Flyers', 'Incline Dumbbell Flyers', 'Decline Dumbbell Flyers', 'Peck Deck Machine', 'Cable Crosovers/ Cable Flyers'],
+//     'Back': ['Pull-Up', 'Chin-Up', 'Lat Pull-Down', 'Bent Over Barbell Rows', 'Bent Over Dumbbell Rows', 'T-Bar Rows', 'Seated Cable Rows', 'Chest Supported Barbell Rows', 'Chest Supported Dumbbell Rows', 'Chest Supported Machine Rows', 'Inverted Rows', 'Barbell, Dumbbell Shrugs', 'Machine Shrugs'],
+//     'Shoulder': ['Seated Overhead Barbell Press', 'Seated Overhead Dumbbell Press', 'Standing Overhead Barbell Press', 'Standing Overhead Dumbbell Press', 'Overhead Machine Press', 'Arnold Press', 'Barbell Upright Rows', 'Dumbbell Upright Rows', 'Machine Upright Rows', 'Dumbbell Lateral Raises', 'Cable Lateral Raises', 'Machine Lateral Raises', 'Dumbbell Front Raises', 'Cable Front Raises', 'Machine Front Raises'],
+//     'Quadriceps': ['Barbell Squats', 'Dumbbell Squats', 'Barbell Front-Squats', 'Dumbell Front-Squats', 'Barbell Split-Squats', 'Dumbbell Split-Squats', 'Barbell Lunges', 'Dumbbell Lunges', 'Barbell Step-Up', 'Dumbbell Step-Up', 'Leg Press', 'Single Leg Press', 'Machine Squat', 'Leg Extensions'],
+//     'Hamstring' : ['Barbell Romanian Deadlifts', 'Dumbbell Romanian Deadlifts', 'Barbell Straight Leg Deadlifts', 'Dumbbell Straight Leg Deadlifts', 'Barbell Sumo Deadlifts', 'Dumbbell Sumo Deadlifts', 'Glute-Ham Raises', 'Hyperextensions', 'Cable Pull-Throughs', 'Good-Mornings', 'Leg Curls'],
+//     'Biceps' : ['Standing Barbell Curls', 'Standing Dumbbell Curls', 'Barbell Preacher Curls', 'Dumbbell Preacher Curls', 'Seated Dumbbell Curls', 'Incline Dumbbell Curls', 'Hammer Curls', 'Concentration Curls', 'Cable Curls', 'Biceps Curl Machine'],
+//     'Triceps' : ['Dips', 'Flat Close Grip Bench Press', 'Decline Close Grip Bench Press', 'Close Grip Push-Ups', 'Laying Barbell Triceps Extensions', 'Laying Dumbbell Triceps Extensions', 'Skull Crushers', 'Overhead Barbell Triceps Extensions', 'Overhead Dumbbell Triceps Extensions', 'Cable Press-Downs', 'Bench Dips']
+// }]
+
 
 let today = new Date();
 let currentMonth = today.getMonth();
@@ -67,13 +93,19 @@ function showCalendar(month, year) {
 
             else {
                 let cell = document.createElement("td");
+
                 // creating a modal for each cell
                 cell.setAttribute('id', (date + '-' + currentMonth + '-' + currentYear));
                 cell.setAttribute('data-target', 'modal fade modal-dialog');
-                cell.setAttribute('data-toggle', "modal")
-                cell.onclick = function () { largerTD(); }
+                cell.setAttribute('data-toggle', "modal");
+                cell.setAttribute('data-modal', (date + '-' + currentMonth + '-' + currentYear + '-modal'));
+
+                // calling onclick function
+                cell.onclick = largerTD;
 
                 let cellText = document.createTextNode(date);
+
+                //Large Modal
                 let modalLG = document.createElement('div');
 
                 setAttributes(modalLG,
@@ -86,6 +118,16 @@ function showCalendar(month, year) {
                         'aria-labelledby': "myLargeModalLabel",
                         'aria-hidden': "true"
                     });
+
+                let modalHeader = document.createElement('div');
+                modalHeader.setAttribute('class', ('bg-secondary p-1'));
+                modalHeader.setAttribute('id', 'modalHeader');
+                let modalHeaderH5 = document.createElement('h5');
+                $(modalHeaderH5).text('Create your Work Out');
+
+                modalLG.prepend(modalHeader);
+                modalHeader.append(modalHeaderH5);
+
                 let modalDialog = document.createElement('div');
                 modalDialog.setAttribute('class', "modal-dialog modal-lg");
 
@@ -95,12 +137,19 @@ function showCalendar(month, year) {
                 // Modal content Dropdown creation
                 let labelSelectWorkOut = document.createElement('label');
                 labelSelectWorkOut.setAttribute('id', (date + '-' + currentMonth + '-' + currentYear + '-label'));
-                console.log(labelSelectWorkOut)
                 $('#' + date + '-' + currentMonth + '-' + currentYear + '-label').text('Select WorkOut');
 
                 // Modal Select Creation
 
-                let selectWorkOut = document.createElement('select')
+                let workOutStyle = document.createElement('h6');
+                workOutStyle.setAttribute('class', 'mt-3 mb-0')
+                $(workOutStyle).text('Select Work Out Style:')
+
+
+                let selectWorkOut = document.createElement('select');
+                selectWorkOut.setAttribute('id', 'modalSelectUp')
+                modalLG.append(workOutStyle)
+                // selectWorkOut.text('placeholder', 'Choose Work Out')
                 labelSelectWorkOut.setAttribute('id', (date + '-' + currentMonth + '-' + currentYear + '-select'));
                 $('#' + date + '-' + currentMonth + '-' + currentYear + '-select').text('Select WorkOut');
 
@@ -108,10 +157,21 @@ function showCalendar(month, year) {
                 for (let k = 0; k < Workouts.length; k++) {
                     let workoutOption = document.createElement('option')
                     workoutOption.setAttribute("id", (date + '-' + currentMonth + '-' + currentYear + Workouts[k]));
-                    workoutOption.text = Workouts[k]
+
+                    workoutOption.text = Workouts[k];
                     selectWorkOut.append(workoutOption);
                     modalContent.appendChild(selectWorkOut);
+
+                    if (workoutOption === 'Crossfit') {
+                        alert('ok')
+                        for (let h = 0; h < CrossfitArr.length; h++) {
+                            let crossfitOption = document.createElement('option')
+                            crossfitOption.text = CrossfitArr[h];
+                            workoutOption.append(crossfitOption);
+                        }
+                    }
                 }
+
 
                 // for (let h = 0; h < Workouts.length; h++){
                 //    let workoutOption = document.createElement('option').text = Workouts[i]
@@ -121,15 +181,16 @@ function showCalendar(month, year) {
 
 
                 // creating closing button for modal
-                // let closeModalBtn = document.createElement('button')
-                // closeModalBtn.setAttribute('id', 'closeModalBtn')
-                // closeModalBtn.setAttribute('class', 'Btn')
-                // closeModalBtn.innerHTML = "Close"
+                let closeModalBtn = document.createElement('button')
+                closeModalBtn.setAttribute('id', 'closeModalBtn')
+                closeModalBtn.setAttribute('class', 'Btn')
+                closeModalBtn.setAttribute('data-dismiss', "modal")
+                closeModalBtn.innerHTML = "Close"
 
-                // modalContent.appendChild(closeModalBtn);
+                modalContent.appendChild(closeModalBtn);
                 modalDialog.appendChild(modalContent);
                 modalLG.appendChild(modalDialog);
-                cell.appendChild(modalLG);
+                document.getElementsByTagName('body')[0].appendChild(modalLG);
 
                 if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
                     cell.classList.add("bg-primary");
@@ -141,11 +202,23 @@ function showCalendar(month, year) {
 
 
                 // function to make modal appear
-                function largerTD() {
-                    $('.modal').modal('show')
+                function largerTD(e) {
+                    let clickedDate = e.path[0];
+                    let modalId = clickedDate.getAttribute('data-modal');
+                    // console.log(e);
+                    $(`#${modalId}`).modal('show');
+                    closeModalBtn.onclick = smallerTD
                 }
-            }
 
+                // function to hide modal
+                function smallerTD(e) {
+                    // alert('clicked')
+                    let clickedDate = e.path[0];
+                    let modalId = clickedDate.getAttribute('data-modal');
+                    $(`#${modalId}`).modal('toggle');
+                }
+
+            }
 
         }
 
@@ -154,28 +227,10 @@ function showCalendar(month, year) {
 
 }
 
-// let table = document.getElementById('calendar');
-// let cellID = document.getElementById(date + '-' + currentMonth + '-' + currentYear);
-// for (let l = 0; l < cellID.length; l++) {
-//     if (cellID[l] !== "") {
-//         cellID[l].onclick = function () { largerTD(); };
-
-//     }
-
-// }
-
-//function for setting multiple attributes to a singel element
+//function for setting multiple attributes to a single element
 function setAttributes(el, attrs) {
     for (var key in attrs) {
         el.setAttribute(key, attrs[key]);
     }
 }
 
-// closing modal button function
-
-function smallerTD() {
-    $('#' +date + '-' + currentMonth + '-' + currentYear + '-modal').modal('hide')
-}
-// document.getElementById('closeModalBtn').addEventListener('click', function(){
-//     $('.modal').modal('hide')
-// })
